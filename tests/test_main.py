8b883f866338bd8824ac60f22267adfaecebac9d
rodrigmars <rodrigmars@gmail.com>
2023-02-26 01:58:28 -0300

    Correções de ambiente

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,24 +4,21 @@ from os import system, path
 
 from settings import NAME_PROJECT, PATH_PROJECT
 
+
 @fixture
 def setup():
 
-    print("PATH_PROJECT:", PATH_PROJECT)
-
     if path.exists(path.join(PATH_PROJECT, NAME_PROJECT)):
 
-        system(f"rm -r {PATH_PROJECT}/{NAME_PROJECT}")        
+        system(f"rm -r {PATH_PROJECT}/{NAME_PROJECT}")
 
     return PATH_PROJECT, NAME_PROJECT
 
 
-def test_create_folder(setup):
+def test_creating_root_folder(setup):
 
-    pass
-    
-    # path_project, name_project = setup
+    path_project, name_project = setup
 
-    # system(f"mkdir {path_project}/{name_project}")
+    system(f"mkdir {path_project}/{name_project}")
 
-    # assert path.isdir(f"{path_project}/{name_project}")
+    assert path.exists(f"{path_project}/{name_project}")
