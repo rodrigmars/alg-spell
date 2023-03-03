4374c334c34a3fbb450b2951e6867870e38b425a
rodrigmars <rodrigmars@gmail.com>
2023-03-03 16:05:40 -0300

    Test performance

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,8 +1,47 @@
+from datetime import datetime
 from re import sub
 from time import process_time
 from pytest import fixture
 from typing import Iterator
 from unicodedata import normalize
+
+
+symbols = {"A": "Alfa",
+           "B": "Bravo",
+           "C": "Charlie",
+           "D": "Delta",
+           "E": "Echo",
+           "F": "Foxtrot",
+           "G": "Golf",
+           "H": "Hotel",
+           "I": "India",
+           "J": "Juliett",
+           "K": "Kilo",
+           "L": "Lima",
+           "M": "Mike",
+           "N": "November",
+           "O": "Oscar",
+           "P": "Papa",
+           "Q": "Quebec",
+           "R": "Romeo",
+           "S": "Sierra",
+           "T": "Tango",
+           "U": "Uniform",
+           "V": "Victor",
+           "W": "Whiskey",
+           "X": "Xray",
+           "Y": "Yankee",
+           "Z": "Zulu",
+           "0": "ZE-RO",
+           "1": "WUN",
+           "2": "TOO",
+           "3": "TREE",
+           "4": "FOW-er",
+           "5": "FIFE",
+           "6": "SIX",
+           "7": "SEV-en",
+           "8": "AIT",
+           "9": "NIN-er"}
 
 
 def only_alphabetic_characters(text: str) -> str:
@@ -35,7 +74,7 @@ def test_letter_to_index_with_enumerate(setup) -> None:
 
     words = setup
 
-    print("words:", words)
+    print("words:>>>>>>>>>", words)
 
     start_time = process_time()
 
@@ -59,3 +98,149 @@ def test_letter_to_index_with_enumerate(setup) -> None:
 
     for k, v in letters.items():
         print(f'{k.rjust(16)}:{v}')
+
+
+def test_get_time_execution():
+
+    phrase = "The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio The NATO phonetic alphabet is a Spelling Alphabet \
+        a set of words used instead of letters in oral communication \
+            over the phone or military radio"
+    print()
+
+    start_time = datetime.now()
+    # list_ = []
+    for word in phrase.split():
+        # print(
+        #     f"{word.rjust(13)}:{list(map(lambda x: symbols[x.upper()], [*word]))}")
+        map(lambda x: symbols[x.upper()], [*word])
+
+    end_time = datetime.now()
+
+    print(f'TIME-01: {(end_time - start_time)}')
+
+    start_time = datetime.now()
+
+    code_words = [{word: [symbols[letter]
+                          for letter in [*word.upper()]]}
+                  for word in phrase.split()]
+
+    # for coded in code_words:
+    #     for k, v in coded.items():
+    #         print(f"{k.rjust(15)}:{v}")
+
+    end_time = datetime.now()
+
+    print(f'TIME-02: {(end_time - start_time)}')
+
+    # non_alphabet = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
+
+    # non_alphabet = {"ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ"}
+
+    non_alphabet = {"Á": "A",
+                    "Â": "A",
+                    "Ã": "A",
+                    "Ä": "A",
+                    "Å": "A",
+                    "Æ": "A",
+                    "Ç": "C",
+                    "È": "E",
+                    "É": "E",
+                    "Ê": "E",
+                    "Ë": "E",
+                    "Ì": "I",
+                    "Í": "I",
+                    "Î": "I",
+                    "Ï": "I",
+                    "Ð": "D",
+                    "Ñ": "N",
+                    "Ò": "O",
+                    "Ó": "O",
+                    "Ô": "O",
+                    "Õ": "O",
+                    "Ö": "O",
+                    "Ø": "O",
+                    "Ù": "U",
+                    "Ú": "U",
+                    "Û": "U",
+                    "Ü": "U",
+                    "Ý": "Y",
+                    "Ŕ": "R",
+                    "Þ": "s",
+                    "ß": "B",
+                    "à": "a",
+                    "á": "a",
+                    "â": "a",
+                    "ã": "a",
+                    "ä": "a",
+                    "å": "a",
+                    "æ": "a",
+                    "ç": "c",
+                    "è": "e",
+                    "é": "e",
+                    "ê": "e",
+                    "ë": "e",
+                    "ì": "i",
+                    "í": "i",
+                    "î": "i",
+                    "ï": "i",
+                    "ð": "o",
+                    "ñ": "n",
+                    "ò": "o",
+                    "ó": "o",
+                    "ô": "o",
+                    "õ": "o",
+                    "ö": "o",
+                    "ø": "o",
+                    "ù": "u",
+                    "ú": "u",
+                    "û": "u",
+                    "ü": "u",
+                    "ý": "y",
+                    "þ": "b",
+                    "ÿ": "y",
+                    "ŕ": "r"}
+
+    def get_alphabet(letter: str) -> str:
+        result = non_alphabet.get(letter)
+        return result if result else sub("[^A-Za-z0-9]+", "", str(letter))
+
+    phrase = "Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein...Fórmula 1: Pérez é o mais rápido no 1º treino livre do GP do Bahrein..."
+
+    words = []
+
+    start_time = datetime.now()
+
+    for word in phrase.split():
+        letters = []
+
+        for letter in list(word):
+
+            if (alpha := get_alphabet(letter)):
+                letters.append(symbols[alpha.upper()])
+
+        words.append([word, letters])
+
+    end_time = datetime.now()
+
+    for word in words:
+        a, b = [*word]
+        print(f"{a.rjust(10)}:{b}")
+
+    print(f'TIME-03: {(end_time - start_time)}')
+    print(len(phrase))
